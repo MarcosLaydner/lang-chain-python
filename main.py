f55a902d78e77b58da533d81cf2f933bbea7be25
MarcosLaydner <marcoslaydner123@gmail.com>
2026-03-16 14:37:05 -0300

    added a chain for processing prompt and response

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from langchain_openai import ChatOpenAI
 from langchain.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
 import os
 
@@ -8,49 +9,22 @@ api_key = os.getenv("OPENAI_API_KEY")
 
 days = 7
 children = 2
-interests = "praia"
+interest = "praia"
 
 prompt_template = PromptTemplate(
     template="""
-        Crie um roteiro de viagem de {days}
-        dias para uma familia com {children} crianças,
-        que gosta de {interests}.
-        Seja breve em suas respostas, e forneça apenas o essencial para cada dia, sem detalhes adicionais.
-    """
+        Sugira uma cidade dado o meu interesse por {interest}
+    """,
+    input_variables=["interest"],
 )
-
-prompt = prompt_template.format(
-    days=days, children=children, interests=interests
-)
-
-print("Prompt gerado: \n", prompt)
 
 model = ChatOpenAI(model="gpt-5-nano", temperature=1, api_key=api_key)
 
-response = model.invoke(prompt)
-
-print(response.content)
+chain = prompt_template | model | StrOutputParser()
 
 
-# Directly with OPENAI
+response = chain.invoke({
+    "interest": interest
+})
 
-# client = OpenAI(api_key=api_key)
-
-#  prompt = f"Crie um roteiro de viagem de {days} dias para uma familia com {children} crianças, que gosta de {interests}. Seja breve em suas respostas, e forneça apenas o essencial para cada dia, sem detalhes adicionais."
-# # try gpt-5-mini (a bit better) OR gpt-5-nano (cheaper)
-# resposta = client.chat.completions.create(
-#     model="gpt-5-mini",
-#     messages=[
-#         {
-#             "role": "system",
-#             "content": "Você é um assistente de planejamento de viagens, que cria roteiros personalizados com base nas preferências do usuário."
-#         },
-#         {
-#             "role": "user",
-#             "content": prompt
-#         }
-#     ]
-# )
-
-
-# print(resposta.choices[0].message.content)
+print(response)
