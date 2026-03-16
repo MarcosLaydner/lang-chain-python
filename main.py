ad72eb2acc33420745d15daa77c437e72b13a286
MarcosLaydner <marcoslaydner123@gmail.com>
2026-03-16 14:49:10 -0300

    parsing output with JsonOutputParser

--- main.py
+++ main.py
@@ -1,30 +1,39 @@
 from langchain_openai import ChatOpenAI
 from langchain.prompts import PromptTemplate
-from langchain_core.output_parsers import StrOutputParser
+from langchain_core.output_parsers import JsonOutputParser
+from langchain.globals import set_debug
+from pydantic import Field, BaseModel
 from dotenv import load_dotenv
 import os
+
+set_debug(True)
 
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
 
-days = 7
-children = 2
-interest = "praia"
+class Destination(BaseModel):
+    city: str = Field(description="A city that matches the user's interest")
+    reason: str = Field(description="A brief explanation of why this city is a good match for the user's interest")
+
+
+parser = JsonOutputParser(pydantic_object=Destination)
 
 prompt_template = PromptTemplate(
     template="""
-        Sugira uma cidade dado o meu interesse por {interest}
+        Sugira uma cidade dado o meu interesse por {interest}.
+        {format_instructions}
     """,
     input_variables=["interest"],
+    partial_variables={"format_instructions": parser.get_format_instructions()}
 )
 
 model = ChatOpenAI(model="gpt-5-nano", temperature=1, api_key=api_key)
 
-chain = prompt_template | model | StrOutputParser()
+chain = prompt_template | model | parser
 
 
 response = chain.invoke({
-    "interest": interest
+    "interest": "praia"
 })
 
 print(response)
