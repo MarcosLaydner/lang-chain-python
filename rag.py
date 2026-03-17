ea36c1b4de9c9edcdcf7881811edc1442116c763
MarcosLaydner <marcoslaydner123@gmail.com>
2026-03-17 13:24:23 -0300

    implemented use of rag for a txt file

---
+++ rag.py
@@ -0,0 +1,44 @@
+from dotenv import load_dotenv
+import os
+
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_community.document_loaders import TextLoader
+from langchain_community.vectorstores import FAISS
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+
+model = ChatOpenAI(model="gpt-5-nano", temperature=1, api_key=api_key)
+
+embeddings = OpenAIEmbeddings()
+
+document = TextLoader("documents/GTB_gold_Nov23.txt", encoding="utf-8").load()
+
+pieces = RecursiveCharacterTextSplitter(
+    chunk_size=1000, chunk_overlap=100
+).split_documents(document)
+
+data = FAISS.from_documents(pieces, embeddings).as_retriever(search_kwargs={"k": 2})
+
+prompt_insurance_consulting = ChatPromptTemplate.from_messages(
+    [
+        ("system", "Responda usando exclusivamente o conteúdo fornecido"),
+        ("human", "{query}\n\nContexto: \n{contexto}\n\nResposta:")
+    ]
+)
+
+chain = prompt_insurance_consulting | model | StrOutputParser()
+
+def respond(query: str):
+    parts = data.invoke(query)
+    context = "\n\n".join([part.page_content for part in parts])
+    return chain.invoke({
+        "query": query,
+        "contexto": context
+    })
+
+
+print(respond("como devo proceder caso tenha um item roubado?"))
