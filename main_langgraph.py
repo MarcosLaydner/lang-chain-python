181308dfa6ba9c57f095f342fdcbfa20a82e9621
MarcosLaydner <marcoslaydner123@gmail.com>
2026-03-17 11:18:45 -0300

    created assistant orchestrator based on

--- main_langgraph.py
+++ main_langgraph.py
@@ -3,6 +3,11 @@ from langchain.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import JsonOutputParser, StrOutputParser
 from dotenv import load_dotenv
 import os
+from typing import Literal, TypedDict
+
+
+class Route(TypedDict):
+    destino: Literal["praia", "montanha"]
 
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
@@ -13,13 +18,38 @@ model = ChatOpenAI(
     api_key=api_key
 )
 
-prompt_consulting = ChatPromptTemplate.from_messages(
+prompt_beach_consulting = ChatPromptTemplate.from_messages(
     [
-        ("system", "Você é um consultor de viagens"),
+        ("system", "Apresente-se como Sra Praia. Você é uma especialista em viagens com destinos para praia. Responda brevemente e de forma objetiva."),
         ("human", "{query}")
     ]
 )
 
-assistant = prompt_consulting | model | StrOutputParser()
+prompt_mountain_consulting = ChatPromptTemplate.from_messages(
+    [
+        ("system", "Apresente-se como Sr Montanha. Você é um especialista em viagens com destinos para montanhas e atividades radicais. Responda brevemente e de forma objetiva"),
+        ("human", "{query}")
+    ]
+)
 
-print(assistant.invoke({"query": "Quero férias em praias no Brasil?"}))
+beach_chain = prompt_beach_consulting | model | StrOutputParser()
+mountain_chain = prompt_mountain_consulting | model | StrOutputParser()
+
+router_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "Responda apenas com 'praia' ou 'montanha'"),
+        ("human", "{query}")
+    ]
+)
+
+router = router_prompt | model.with_structured_output(Route)
+
+def response(query: str):
+    route = router.invoke({"query": query})["destino"]
+    if route == "praia":
+        return beach_chain.invoke({"query": query})
+    else:
+        return mountain_chain.invoke({"query": query})
+
+
+print(response("Quero me aventurar em lugar alto"))
